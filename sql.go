492b30d6e19ef7ef911fd006ce4f68fa9b7c3851
imgurbot12 <imgurbot12@gmail.com>
2017-09-14 16:16:22 -0700

    additional functions for fwrules and defaults

--- sql.go
+++ sql.go
@@ -2,17 +2,56 @@ package goaway2
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/mattn/go-sqlite3" //mysql-driver
 )
 
-//TODO: need to change database location for when its compiled to something else
+//TODO:50 need to change database location for when its compiled to something else
 
 /***Varaibles***/
 var db *sql.DB
 
 /***Functions***/
+
+//sqlLoadRules : load all firewall rules from database
+func sqlLoadRules() (fwRules []*fwRule) {
+	// do sql query
+	rows, err := db.Query("SELECT Zone,FromIP,FromPort,ToIP,ToPort FROM rules ORDER BY RuleNum")
+	if err != nil {
+		fmt.Printf("Unable to collect firewall Rules! SQL-Error: %s", err.Error())
+		os.Exit(1)
+	}
+	// fill rules with given data
+	var rec *fwRaw
+	for rows.Next() {
+		rec = new(fwRaw)
+		rows.Scan(&rec.Zone, &rec.FromIP, &rec.FromPort, &rec.ToIP, &rec.ToPort)
+		// build rule with types based on data from sql table
+		fwRules = append(fwRules, &fwRule{
+			Zone:    zone(rec.Zone),
+			SrcIP:   convertIPs(rec.FromIP),
+			SrcPort: convertPorts(rec.FromPort),
+			DstIP:   convertIPs(rec.ToIP),
+			DstPort: convertPorts(rec.ToPort),
+		})
+	}
+	rows.Close()
+	return fwRules
+}
+
+//sqlLoadDefaults : load rule options into defaults
+func sqlLoadDefaults() (df *dfaults) {
+	// do sql query and scan data
+	err := db.QueryRow("SELECT Inbound, OutBound FROM ruleopts LIMIT 1").Scan(&df.inbound, &df.outbound)
+	if err != nil {
+		fmt.Printf("Unable to collect firewall options! SQL-Error: %s", err.Error())
+		os.Exit(1)
+	}
+	return df
+}
 
 //checkExists : check if given database exists
 func checkExists(db *sql.DB, table string) {
@@ -36,5 +75,7 @@ func init() {
 	checkExists(db, "rules")
 	checkExists(db, "whitelist")
 	checkExists(db, "blacklist")
+	// load fw rules from sql table
+	sqlLoadRules()
 	db.Close()
 }
