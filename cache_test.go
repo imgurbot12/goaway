9e3646d1607d3ace63e85d24c02e3d23e0700d35
IHaxYou <andrewcscott007@gmail.com>
2018-01-20 13:21:08 -0700

    extended cache testing

--- cache_test.go
+++ cache_test.go
@@ -5,17 +5,27 @@ import (
 	"testing"
 )
 
-/***Variables***/
-var cache = NewRedBlackTree()
-var kv = NewRedBlackKV()
+/* Variables */
+var rbc = NewRedBlackTree()
+var rbkv = RBKV{}
 
-/***Benchmarks***/
+/* Benchmarks */
 
-func BenchmarkCache(b *testing.B) {
+func BenchmarkCacheRedBlackSET(b *testing.B) {
 	var i int64
+	var key string
 	for i = 0; i < int64(b.N); i++ {
-		key := strconv.FormatInt(i, 10)
-		cache.Set(kv, key, "fuck my shit")
+		key = strconv.FormatInt(i, 10)
+		rbc.Set(rbkv, key, "fuck my shit")
+	}
+}
+
+func BenchmarkCacheRedBlackGET(b *testing.B) {
+	var i int64
+	var key string
+	for i = 0; i < int64(b.N); i++ {
+		key = strconv.FormatInt(i, 10)
+		rbc.Get(rbkv, key)
 	}
 }
 
